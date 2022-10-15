fa167e8d5b7ad38a73a5c795e02694c9a55304ba
LittleFive <newyue2010@163.com>
2022-10-15 13:54:50 +0800

    wip

--- src/simpleCalculator.cpp
+++ src/simpleCalculator.cpp
@@ -15,6 +15,7 @@
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+// #include <inputHandle.hpp>
 #include <iostream>
 #include <list>
 #include <map>
@@ -25,9 +26,13 @@
 
 using namespace std;
 
-const string symbol = "+-*/$^!%()";
+const string symbol = "+-*/^$!%()";
+const string biOperator = "+-*/^%";
 
 map<string, string> customVarible;
+
+string calculate(string, string);
+string calculate(string, string, string);
 
 int calcuLevel(string symbol) {
   if (symbol.compare("+") == 0 || symbol.compare("-") == 0) {
@@ -43,127 +48,193 @@ int calcuLevel(string symbol) {
 }
 
 int main() {
-  std::cout << "Welcome to simple calculator! \nPlease input your expression: "
+  std::cout << "Welcome to simple calculator! Type exit to end calculate. "
+               "\nPlease input your expression: "
+            //  "\nhint: negative numbers please add parentheses: "
             << endl;
 
   // Read input string and convert to vector.
   string input = "";
   cin >> input;
-  vector<string> seglist;
-  size_t prev = 0, pos;
-  while ((pos = input.find_first_of(symbol, prev)) != string::npos) {
-    // std::cout << "pos = " << pos << " prev = " << prev << endl;
-    if (pos > prev) {
-      seglist.push_back(input.substr(prev, pos - prev));
-      seglist.push_back(input.substr(pos, 1));
-    } else if (pos == prev) {
-      seglist.push_back(input.substr(pos, 1));
+  while (input.compare("exit") == 0) {
+    // assign function
+    if (int equalSymbol = input.find("=")) {
+      string variable = input.substr(0, equalSymbol);
+      string expression = input.substr(equalSymbol + 1, input.length());
+      if (!regex_match(variable, regex("A-z")) ||
+          customVarible.find(variable) != customVarible.end()) {
+        std::cout << "Invalid input!" << endl;
+        continue;
+      } else {
+        customVarible.insert(pair<string, string>(variable, expression));
+        //  TODO: expression calculate
+      }
     }
-    prev = pos + 1;
-  }
-  if (prev < input.length()) {
-    seglist.push_back(input.substr(prev, string::npos));
-  }
-  // for (int i = 0; i < seglist.size(); i++) {
-  //   std::cout << seglist[i];
-  // }
-  // std::cout << endl;
 
-  // sqrt / negative sign transfer and input check
-  string invaildSymbolCombo = "+-*/^";
-  for (int i = 0; i < seglist.size(); i++) {
-    // std::cout << i << ": " << seglist[i] << endl;
-    if (seglist[i].compare("sqrt") == 0) {
-      if (i < seglist.size() - 1 &&
-          symbol.find(seglist[i + 1]) == string::npos) {
-        // std::cout << "hit!" << endl;
-        seglist[i] = "$";
-        seglist.erase(seglist.begin() + i + 1);
-        seglist.erase(seglist.begin() + i + 2);
-      } else {
-        std::cout << "Invalid input!" << endl;
-        // continue;
+    // general situation
+    vector<string> seglist;
+    size_t prev = 0, pos;
+    while ((pos = input.find_first_of(symbol, prev)) != string::npos) {
+      // std::cout << "pos = " << pos << " prev = " << prev << endl;
+      if (pos > prev) {
+        seglist.push_back(input.substr(prev, pos - prev));
+        seglist.push_back(input.substr(pos, 1));
+      } else if (pos == prev) {
+        seglist.push_back(input.substr(pos, 1));
+      }
+      prev = pos + 1;
+    }
+    if (prev < input.length()) {
+      seglist.push_back(input.substr(prev, string::npos));
+    }
+    // for (int i = 0; i < seglist.size(); i++) {
+    //   std::cout << seglist[i];
+    // }
+    // std::cout << endl;
+
+    bool validCheck = true;
+    // sqrt / negative sign transfer
+    for (int i = 0; i < seglist.size(); i++) {
+      // std::cout << i << ": " << seglist[i] << endl;
+      if (seglist[i].compare("sqrt") == 0) {
+        if (i < seglist.size() - 1 &&
+            symbol.find(seglist[i + 1]) == string::npos) {
+          // std::cout << "hit!" << endl;
+          seglist[i] = "$";
+          seglist.erase(seglist.begin() + i + 1);
+          seglist.erase(seglist.begin() + i + 2);
+        } else {
+          std::cout << "Invalid input!" << endl;
+          validCheck = false;
+          break;
+        }
+      }
+      if (seglist[i].compare("-") == 0 &&
+          (i == 0 || biOperator.find(seglist[i - 1]) != string::npos)) {
+        if (i < seglist.size() - 1 &&
+            biOperator.find(seglist[i + 1]) == string::npos) {
+          seglist[i + 1] = "-" + seglist[i + 1];
+          seglist.erase(seglist.begin() + i);
+          i--;
+        } else {
+          std::cout << "Invalid input!" << endl;
+          validCheck = 0;
+          break;
+        }
       }
     }
-    if (seglist[i].compare("-") == 0) {
-      if (i < seglist.size() - 1 &&
-          invaildSymbolCombo.find(seglist[i + 1]) == string::npos) {
-        seglist[i + 1] = "-" + seglist[i + 1];
-        seglist.erase(seglist.begin() + i);
-        i--;
-      } else {
-        std::cout << "Invalid input!" << endl;
+    if (!validCheck) {
+      continue;
+    }
+    // input check
+    for (int i = 0; i < seglist.size(); i++) {
+      if (!regex_match(
+              seglist[i],
+              regex("(\\-){0,1}([0-9]+)(\\.[0-9]+){0,1}(\\e[0-9]+){0,1}",
+                    regex::icase)) &&
+          symbol.find(seglist[i]) == string::npos) {
+        map<string, string>::iterator l_it;
+        l_it = customVarible.find(seglist[i]);
+        if (l_it == customVarible.end()) {
+          cout << "Invalid input!" << endl;
+          validCheck = false;
+          break;
+        }
       }
     }
-  }
-  for (int i = 0; i < seglist.size(); i++) {
-    if (!regex_match(seglist[i],
-                     regex("(\\-){0,1}([0-9]+)(\\.[0-9]+){0,1}(\\e[0-9]+){0,1}",
-                           regex::icase))) {
-      map<string, string>::iterator l_it;
-      l_it = customVarible.find(seglist[i]);
-      if (l_it == customVarible.end()) {
+    if (!validCheck) {
+      continue;
+    }
+    // format check
+    for (int i = 0; i < seglist.size() - 1; i++) {
+      if (i == 0 && symbol.find(seglist[i]) != string::npos) {
         cout << "Invalid input!" << endl;
-        // continue;
+        validCheck = false;
+        break;
+      }
+      if (symbol.find(seglist[i]) != string::npos &&
+          seglist[i + 1].compare("-") != 0) {
+        cout << "Invalid input!" << endl;
+        validCheck = false;
+        break;
+      }
+      if (i > 0 && symbol.find(seglist[i - 1]) != string::npos &&
+          symbol.find(seglist[i]) != string::npos &&
+          symbol.find(seglist[i + 1]) != string::npos) {
+        cout << "Invalid input!" << endl;
+        validCheck = false;
+        break;
       }
     }
-  }
-
-  // for (int i = 0; i < seglist.size() - 1; i++) {
-  //   if (seglist[i].compare("-") == 0) {
-  //     if ((i == 0 || symbol.find(seglist[i - 1]) != string::npos) &&
-  //         i < seglist.size() - 1 &&
-  //         symbol.find(seglist[i + 1]) == string::npos) {
-  //       seglist[i + 1] = "-" + seglist[i + 1];
-  //       seglist.erase(seglist.begin() + i);
-  //       i--;
-  //     }
-  //   }
-  // }
-  // for (int i = 0; i < seglist.size(); i++) {
-  //   std::cout << seglist[i];
-  // }
-
-  // Infix expression to suffix expression
-  list<string> suffix;        // s2
-  stack<string> symbolStack;  // s1
-  for (int i = 0; i < seglist.size(); i++) {
-    if (symbol.find(seglist[i]) == string::npos) {
-      // if (regex_match(
-      //         seglist[i],
-      //         regex("(\\-){0,1}([0-9]+)(\\.[0-9]+){0,1}(\\e[0-9]+){0,1}",
-      //               regex::icase))) {
-      // } else {
-      //   cout << "Invalid input!" << endl;
-      //   // continue;
-      // }
-      suffix.push_back(seglist[i]);
+    if (!validCheck) {
+      continue;
     }
-    if (seglist[i].compare("(") == 0) {
-      symbolStack.push(seglist[i]);
-    } else if (seglist[i].compare(")") == 0) {
-      while (symbolStack.top().compare("(") != 0) {
-        suffix.push_back(symbolStack.top());
+    // for (int i = 0; i < seglist.size() - 1; i++) {
+    //   if (seglist[i].compare("-") == 0) {
+    //     if ((i == 0 || symbol.find(seglist[i - 1]) != string::npos) &&
+    //         i < seglist.size() - 1 &&
+    //         symbol.find(seglist[i + 1]) == string::npos) {
+    //       seglist[i + 1] = "-" + seglist[i + 1];
+    //       seglist.erase(seglist.begin() + i);
+    //       i--;
+    //     }
+    //   }
+    // }
+    // for (int i = 0; i < seglist.size(); i++) {
+    //   std::cout << seglist[i];
+    // }
+
+    // Infix expression to suffix expression
+    list<string> suffix;        // s2
+    stack<string> symbolStack;  // s1
+    for (int i = 0; i < seglist.size(); i++) {
+      if (symbol.find(seglist[i]) == string::npos) {
+        suffix.push_back(seglist[i]);
+      }
+      if (seglist[i].compare("(") == 0) {
+        symbolStack.push(seglist[i]);
+      } else if (seglist[i].compare(")") == 0) {
+        while (symbolStack.top().compare("(") != 0) {
+          suffix.push_back(symbolStack.top());
+          symbolStack.pop();
+        }
         symbolStack.pop();
+      } else {
+        while (!symbolStack.empty() &&
+               calcuLevel(seglist[i]) < calcuLevel(symbolStack.top())) {
+          suffix.push_back(symbolStack.top());
+          symbolStack.pop();
+        }
+        symbolStack.push(seglist[i]);
       }
+    }
+    while (!symbolStack.empty()) {
+      suffix.push_back(symbolStack.top());
       symbolStack.pop();
-    } else {
-      while (!symbolStack.empty() &&
-             calcuLevel(seglist[i]) < calcuLevel(symbolStack.top())) {
-        suffix.push_back(symbolStack.top());
-        symbolStack.pop();
-      }
-      symbolStack.push(seglist[i]);
     }
-  }
-  while (!symbolStack.empty()) {
-    suffix.push_back(symbolStack.top());
-    symbolStack.pop();
-  }
 
-  // Calculate start
+    // Calculate start
+    stack<string> calStack;
+    for (int i = 0; i < seglist.size(); i++) {
+      if (symbol.find(seglist[i]) == string::npos) {
+        calStack.push(seglist[i]);
+        continue;
+      }
+      if (biOperator.find(seglist[i]) != string::npos) {
+        string val1 = calStack.top();
+        calStack.pop();
+        string val2 = calStack.top();
+        calStack.pop();
+        calStack.push(calculate(val1, val2, seglist[i]));
+      } else {
+        string val = calStack.top();
+        calStack.pop();
+        calStack.push(calculate(val, seglist[i]));
+      }
+    }
 
-  std::cout << endl;
-  std::cout << "finish" << endl;
+    std::cout << endl;
+    std::cout << "finish" << endl;
+  }
   return 0;
 }
